f1df6a30e13d51a5f7a7421a85e78266e5229c5b
REMSHA TARIQ <remshatariq21@gmail.com>
2025-06-19 18:14:16 +0500

    Add files via upload

---
+++ calculator.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System;
+using System.Numerics;
+class Program
+{
+    
+    static void main()
+    {
+        int a = 10;
+        int b = 20;
+       static  void  AddSubtract(int a, int b )
+        {
+            Console.WriteLine (a + b);
+            Console.WriteLine (a - b);
+        }
+        static void MultiplyDivide:AddSubtract(int a,int b)
+            {
+            Console.WriteLine(a * b);
+            Console.WriteLine(a / b);
+        }
+        static void calculator
+
+    }
+}
+
